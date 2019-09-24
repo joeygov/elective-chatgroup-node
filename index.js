6867e19d041aaf82968920a7019ffa1207b5c45f
joeygov <joey.destacamento@student.passerellesnumeriques.org>
2019-09-24 17:43:23 +0800

    adding socket layout

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 var app = require('express')();
-var http = require('http').createServer(app);
-var port = process.env.PORT || 3000
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8000;
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected ' + socket.client);
+
+});
+
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', function (socket) {
+  socket.on('login', function (username) {
+    io.emit('login', username)
   });
 
-http.listen(port, function(){
-  console.log('listening on *: ' + port);
+  io.on('connection', function (socket) {
+    socket.on('typing', function (username) {
+      io.sockets.emit('typing', username)
+    })
+  })
+});
+
+
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
 });
